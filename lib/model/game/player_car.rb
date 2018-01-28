5be007d6f2cd136478eeec33811678ffb41f32ef
Petr Wudi <petr.wudi@gmail.com>
2018-01-28 11:36:43 +0100

    Add simplified constructor of PlayerCar class

--- lib/model/game/player_car.rb
+++ lib/model/game/player_car.rb
@@ -1,5 +1,10 @@
+require 'model/game/car'
+
 module NeedNotToSpeed
   # Class representing player's car
   class PlayerCar < Car
+    def initialize
+      super(0, 0, nil, 50, 30)
+    end
   end
 end
