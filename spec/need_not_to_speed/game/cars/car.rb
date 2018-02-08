1bbd4768d42947d31e1ecbafbbeee5930c6834cd
Petr Wudi <petr.wudi@gmail.com>
2018-02-08 19:30:50 +0100

    Add car test

---
+++ spec/need_not_to_speed/game/cars/car.rb
@@ -0,0 +1,25 @@
+require_relative '../../../spec_helper'
+require 'model/game/cars/car'
+
+RSpec.describe NeedNotToSpeed::Game::Car do
+  context 'changes it\'s state' do
+    subject(:car) { NeedNotToSpeed::Game::Car.new(0, 0) }
+
+    it 'doesn\'t turn left until it moves front or back' do
+      rotation_start = car.rotation
+      car.state.turning_left = true
+      rotation_end = car.rotation
+      rotation_end += 2 * Math::PI while rotation_end < rotation_start
+      expect(rotation_start).to eq rotation_end
+    end
+
+    it 'doesn\'t turn right until it moves front or back' do
+      rotation_start = car.rotation
+      car.state.turning_right = true
+      rotation_end = car.rotation
+      rotation_end += 2 * Math::PI while rotation_end < rotation_start
+      expect(rotation_start).to eq rotation_end
+    end
+  end
+
+end
